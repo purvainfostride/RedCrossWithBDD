ee019ca7720ef9632535e7abde05f04074c5d84c
purvainfostride <purva@infostride.com>
2022-04-07 21:52:55 +0530

    f

--- Pages/SignupAndLoginPage.cs
+++ Pages/SignupAndLoginPage.cs
@@ -10,42 +10,42 @@ namespace RedCrossWithBDD.Pages
         public void UserEnteredValidFirstName()
         {
             IWebElement firstname = driver.FindElement(By.XPath("(//form[@class='form'])[3]//input[contains(@name, 'customer[first_name]')]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].value='Poorva';", firstname);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='Poorva';", firstname);
         }
         public void UserEnteredValidLastName()
         {
             IWebElement lastname = driver.FindElement(By.Id("customer[last_name]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].value='Malhotra';", lastname);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='Malhotra';", lastname);
         }
         public void UserEnteredValidEmail()
         {
             IWebElement email = driver.FindElement(By.Id("register-customer[email]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].value='purvamalhotra815@gmail.com';", email);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='purvamalhotra815@gmail.com';", email);
         }
         public void UserEnteredValidPassword()
         {
             IWebElement pass = driver.FindElement(By.Id("register-customer[password]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].value='purvamalhotra815@';", pass);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='purvamalhotra815@';", pass);
         }
         public void UserClickedOnCreateMyAccountButton()
         {
             IWebElement create = driver.FindElement(By.XPath("//button[contains(text(),'Create my account')]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].click();", create);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", create);
         }
         public void UserEnteredValidEmailID()
         {
             IWebElement emailID = driver.FindElement(By.Id("login-customer[email]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].value='purvamalhotra815@gmail.com';", emailID);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='purvamalhotra815@gmail.com';", emailID);
         }
         public void UserEnteredValidUserPassword()
         {
             IWebElement userpass = driver.FindElement(By.Id("login-customer[password]"));
-            javaScriptExecutor.ExecuteScript("arguments[0].value='purvamalhotra815@';", userpass);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='purvamalhotra815@';", userpass);
         }
         public void UserClickOnLoginButton()
         {
             IWebElement loginBtn = driver.FindElement(By.XPath("(//form[@class='form'])[1]//button"));
-            javaScriptExecutor.ExecuteScript("arguments[0].click();", loginBtn);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", loginBtn);
         }
     }
 }
