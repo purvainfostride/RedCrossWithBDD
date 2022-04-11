db6b08f33fbbdb0c10d1d297983d720a843eb744
purvainfostride <purva@infostride.com>
2022-04-11 15:43:54 +0530

    REf

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -25,5 +25,85 @@ namespace RedCrossWithBDD.Pages
             Thread.Sleep(2000);
             driver.FindElement(By.PartialLinkText("account")).Click();
         }
+        public void SignupDialogueBoxAppears()
+        {
+            driver.FindElement(By.XPath("//form[@id='header_customer_login']//following-sibling::div[@class='popover__secondary-action']//button[@class='link link--accented']")).Click();
+            // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(9000);
+
+        }
+      
+        public void UserEnteredValidFirstName()
+        {
+            IWebElement firstname = driver.FindElement(By.XPath("(//form[@class='form'])[3]//input[contains(@name, 'customer[first_name]')]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='Poorva';", firstname);
+        }
+        public void UserEnteredValidLastName()
+        {
+            IWebElement lastname = driver.FindElement(By.Id("customer[last_name]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='Malhotra';", lastname);
+        }
+        public void UserEnteredValidEmail()
+        {
+            IWebElement email = driver.FindElement(By.Id("register-customer[email]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@gmail.com';", email);
+        }
+        public void UserEnteredValidPassword()
+        {
+            IWebElement pass = driver.FindElement(By.Id("register-customer[password]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@';", pass);
+
+        }
+        public void UserClickedOnCreateMyAccountButton()
+        {
+            IWebElement create = driver.FindElement(By.XPath("//button[contains(text(),'Create my account')]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", create);
+        }
+
+        public void UserEnteredValidEmailID()
+        {
+            IWebElement emailID = driver.FindElement(By.Id("login-customer[email]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@gmail.com';", emailID);
+        }
+
+
+        public void UserEnteredValidUserPassword()
+        {
+            IWebElement userpass = driver.FindElement(By.Id("login-customer[password]"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@';", userpass);
+        }
+
+        public void UserClickOnLoginButton()
+        {
+            IWebElement loginBtn = driver.FindElement(By.XPath("(//form[@class='form'])[1]//button"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", loginBtn);
+        }
+
+
+        public void UserClickedOnShopeHomeMenuButton()
+        {
+            driver.FindElement(By.PartialLinkText("Shop Home")).Click();
+        }
+
+
+        public void UserClickedOnNewInMenuButton()
+        {
+            driver.FindElement(By.PartialLinkText("New In")).Click();
+        }
+
+        public void UserClickedOnVirtualGiftsMenuButton()
+        {
+            driver.FindElement(By.PartialLinkText("Virtual Gifts")).Click();
+        }
+        public void UserClickedOnSaleMenuButton()
+        {
+            driver.FindElement(By.PartialLinkText("Sale")).Click();
+        }
+
+        public void UserClosedTheBrowser()
+        {
+            Thread.Sleep(2000);
+            driver.Quit();
+
+        }
     }
 }
--- StepDefinitions/HomePageSteps.cs
+++ StepDefinitions/HomePageSteps.cs
@@ -40,38 +40,31 @@ namespace RedCrossWithBDD.StepDefinitions
         [Then(@"Signup dialogue box appears")]
         public void ThenSignupDialogueBoxAppears()
         {
-            driver.FindElement(By.XPath("//form[@id='header_customer_login']//following-sibling::div[@class='popover__secondary-action']//button[@class='link link--accented']")).Click();
-            // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(9000);
+            homePage.SignupDialogueBoxAppears();
 
         }
         [When(@"user entered Valid FirstName")]
         public void WhenUserEnteredValidFirstName()
         {
-            IWebElement firstname = driver.FindElement(By.XPath("(//form[@class='form'])[3]//input[contains(@name, 'customer[first_name]')]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='Poorva';", firstname);
-
-
+            homePage.UserEnteredValidFirstName();
         }
 
         [When(@"user entered Valid LastName")]
         public void WhenUserEnteredValidLastName()
         {
-            IWebElement lastname = driver.FindElement(By.Id("customer[last_name]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='Malhotra';", lastname);
+            homePage.UserEnteredValidLastName();
         }
 
         [When(@"user entered Valid Email")]
         public void WhenUserEnteredValidEmail()
         {
-            IWebElement email = driver.FindElement(By.Id("register-customer[email]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@gmail.com';", email);
+            homePage.UserEnteredValidEmail();
         }
 
         [When(@"user entered Valid Password")]
         public void WhenUserEnteredValidPassword()
         {
-            IWebElement pass = driver.FindElement(By.Id("register-customer[password]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@';", pass);
+            homePage.UserEnteredValidPassword();
 
         }
 
@@ -79,87 +72,68 @@ namespace RedCrossWithBDD.StepDefinitions
         [Then(@"user is able to signup successfully")]
         public void ThenUserClickedOnCreateMyAccountButton()
         {
-            IWebElement create = driver.FindElement(By.XPath("//button[contains(text(),'Create my account')]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", create);
+            homePage.UserClickedOnCreateMyAccountButton();
         }
         [When(@"user entered Valid EmailID")]
         public void WhenUserEnteredValidEmailID()
         {
-            IWebElement emailID = driver.FindElement(By.Id("login-customer[email]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@gmail.com';", emailID);
+            homePage.UserEnteredValidEmailID();
         }
 
         [When(@"user entered Valid UserPassword")]
         public void WhenUserEnteredValidUserPassword()
         {
-            IWebElement userpass = driver.FindElement(By.Id("login-customer[password]"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value='poorvamalhotra815@';", userpass);
+            homePage.UserEnteredValidUserPassword();    
         }
         [Then(@"user click on Login Button")]
         [Then(@"user logged in successfully")]
 
         public void WhenUserClickOnLoginButton()
         {
-            IWebElement loginBtn = driver.FindElement(By.XPath("(//form[@class='form'])[1]//button"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", loginBtn);
+            homePage.UserClickOnLoginButton();
         }
 
         [When(@"user clicked on Shope Home menu button")]
-        public void WhenUserClickedOnShopeHomeMenuButton()
-        {
-            driver.FindElement(By.PartialLinkText("Shop Home")).Click();
-        }
 
         [Then(@"user is on Shope Home page of the application")]
         public void ThenUserIsOnShopeHomePageOfTheApplication()
         {
+            homePage.UserClickedOnShopeHomeMenuButton();
             Assert.AreEqual(url, driver.Url);
         }
 
-
         [When(@"user clicked on New In menu button")]
-        public void WhenUserClickedOnNewInMenuButton()
-        {
-            driver.FindElement(By.PartialLinkText("New In")).Click();
-        }
 
         [Then(@"user is redirected to New Arrivals page")]
         public void ThenUserIsRedirectedToNewArrivalsPage()
         {
+            homePage.UserClickedOnNewInMenuButton();
             Assert.AreEqual(newInPageUrl, driver.Url);
         }
 
         [When(@"user clicked on Virtual Gifts menu button")]
         [Given(@"user clicked on Virtual Gifts menu button")]
-        public void WhenUserClickedOnVirtualGiftsMenuButton()
-        {
-            driver.FindElement(By.PartialLinkText("Virtual Gifts")).Click();
-        }
-
         [Then(@"user is redirected to Virtual Gifts page")]
         [When(@"user is redirected to Virtual Gifts page")]
         public void ThenUserIsRedirectedToVirtualGiftsPage()
         {
+            homePage.UserClickedOnVirtualGiftsMenuButton();
             Assert.AreEqual(virtualGiftsPageUrl, driver.Url);
         }
 
         [When(@"user clicked on Sale menu button")]
-        public void WhenUserClickedOnSaleMenuButton()
-        {
-            driver.FindElement(By.PartialLinkText("Sale")).Click();
-        }
 
         [Then(@"user is redirected to Sale page")]
         public void ThenUserIsRedirectedToSalePage()
         {
+            homePage.UserClickedOnSaleMenuButton();
             Assert.AreEqual(salePageUrl, driver.Url);
         }
 
         [Then(@"user closed the browser")]
         public void ThenUserClosedTheBrowser()
         {
-            Thread.Sleep(2000);
-            driver.Quit();
+            homePage.UserClosedTheBrowser();
             
         }
 
