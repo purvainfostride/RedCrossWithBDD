4805af58e3ed3233d1cf25dd0a62311a990fc634
purvainfostride <purva@infostride.com>
2022-04-13 17:41:09 +0530

    Refractored

--- StepDefinitions/NewInPageStepDefinitions.cs
+++ StepDefinitions/NewInPageStepDefinitions.cs
@@ -9,8 +9,7 @@ namespace RedCrossWithBDD.StepDefinitions
     [Binding]
     public class NewInPageStepDefinitions:BasePage
     {
-        public string newinpageProducts = "//div[@class='product-list product-list--collection product-list--with-sidebar']//a[@class='product-item__title text--strong link']";
-        public string newinpageProductsXpath = "//div[@class='product-item__info-inner']//a";
+        public string newinpageProductsXpath = "//div[@class='product-list product-list--collection product-list--with-sidebar']//a[@class='product-item__title text--strong link']";
         public string nextpageByNumbersXPath = "//div[@class='pagination__nav']//a";
         public string nextbuttonXpath = "(//div[@class='pagination__inner']//a[@title='Next'])";
         public string prevbuttonXpath = "//div[@class='pagination__inner']//a[@title='Previous']";
@@ -18,24 +17,31 @@ namespace RedCrossWithBDD.StepDefinitions
 
         [Then(@"New Arrival's Product count is same as displayed")]
         public void ThenNewArrivalsProductCountIsSameAsDisplayed()
-        {   
-            //IWebElement NewIn = driver.FindElement(By.XPath("//span[@class='collection__showing-count hidden-pocket hidden-lap']"));
-            //Assert.AreEqual("Showing 1 - 36 of 79 products", NewIn.Text.ToString());
-              var newinpageProductsCount=driver.FindElements(By.XPath(newinpageProducts));
-            
-                var totalProductsOfNewInPage = newinpageProductsCount.Count;
-                var nextbynumbers = driver.FindElements(By.XPath(nextpageByNumbersXPath));
+        {
+            var newinpageProduct1 = driver.FindElements(By.XPath(newinpageProductsXpath));
+            var p1 = newinpageProduct1.Count;
+            Console.WriteLine(newinpageProduct1.Count);
+            driver.FindElement(By.XPath(nextbuttonXpath)).Click();
+            Thread.Sleep(2000);
+            var newinpageProduct2 = driver.FindElements(By.XPath(newinpageProductsXpath));
+            var p2 = newinpageProduct2.Count;
+            Console.WriteLine(newinpageProduct2.Count);
+            driver.FindElement(By.XPath(nextbuttonXpath)).Click();
+            Thread.Sleep(2000);
+            var newinpageProduct3 = driver.FindElements(By.XPath(newinpageProductsXpath));
+            var p3 = newinpageProduct3.Count;
+            Console.WriteLine(newinpageProduct3.Count);
+            var p4 = p1 + p2 + p3;
+            Assert.AreEqual(79, p4);
+            //var newinpageProducts = driver.FindElements(By.XPath(newinpageProductsXpath));
+            //for (int i = 0; i <= 2; i++)
+            //{
 
-                for (int i = 1; i <= nextbynumbers.Count; i++)
-                {
-                    IWebElement nextpagebynumbers = driver.FindElement(By.XPath("(" + nextpageByNumbersXPath + ")" + "[" + i + "]"));
-                    nextpagebynumbers.Click();
-                    
-                    var totalProducts = newinpageProductsCount.Count;
-               
-            }
-             //   Assert.AreEqual(79, newinpageProductsCount.Count);
-            
+            //    Console.WriteLine(newinpageProducts.Count);
+            //    driver.FindElement(By.XPath(nextbuttonXpath)).Click();
+            //    Thread.Sleep(2000);
+            //}
+
         }
         [Then(@"New Arrival's Products are clickable")]
         public void ThenNewArrivalsProductsAreClickable()
