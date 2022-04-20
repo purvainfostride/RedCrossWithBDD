ee9dde1b65ef9138b1d839a05fc5cb2f25ff0307
purvainfostride <purva@infostride.com>
2022-04-20 15:03:53 +0530

    Implemented Background tag to reduce step repitions

--- Features/HomePage.feature
+++ Features/HomePage.feature
@@ -1,18 +1,14 @@
-﻿Feature: Checking functionality of HomePage of the Application
+﻿Feature: HomePage
+To Validate functionality of HomePage
 
-@HomePage
-Scenario: To validate whether user is on Landing page of the application of the application
-	Given user initialized the browser
-	When user navigated to the Application
-	Then user is on Landing page of the application
-	And user closed the browser
+Background: 
+Given user is on Landing page of the application
+And user managed the cookies 
 
 @HomePage
 @UserSignup
-Scenario:To validate whether user is able to signup to the Application with Valid details
-	Given user is on Landing page of the application
-	And user managed the cookies 
-	And user clicked on My Account button
+Scenario: User is able to signup to the Application with Valid details
+	Given user clicked on My Account button
 	And Login dialogue box appears
 	And user clicked on Create your account Button
 	And Signup dialogue box appears
@@ -26,10 +22,8 @@ Scenario:To validate whether user is able to signup to the Application with Vali
 
 @HomePage
 @UserLogin
-Scenario: To validate whether the user is able to login with valid credentials
-	Given user is on Landing page of the application
-	And user managed the cookies 
-	And user clicked on My Account button
+Scenario: User is able to login with valid credentials
+	Given user clicked on My Account button
 	And Login dialogue box appears
 	When user entered Valid EmailID
 	And user entered Valid UserPassword
@@ -37,27 +31,8 @@ Scenario: To validate whether the user is able to login with valid credentials
 	And user logged in successfully
 	And user closed the browser
 
-#@HomePage
-#Scenario: To validate the menu bar buttons functionality
-#	Given user is on Landing page of the application
-#	And user managed the cookies 
-#	When user clicked on Shope Home menu button
-#	Then user is on Shope Home page of the application
-#	And user clicked on New In menu button
-#	And user is redirected to New Arrivals page
-#	And user clicked on Clothing menu button
-#	And user clicked on Stationary menu button
-#	And user clicked on Homeware menu button
-#	And user clicked on Beauty menu button
-#	And user clicked on Virtual Gifts menu button
-#	And user is on Virtual Gifts Page
-#	And user clicked on Sale menu button
-#	And user is redirected to Sale page
-#	And user closed the browser
-
 @HomePage
 Scenario: To validate the menu bar buttons functionality
-	Given user is on Landing page of the application
-	And user managed the cookies 
 	When user clicked on menu buttons
+	Then user closed the browser
 	
--- Features/HomePage.feature.cs
+++ Features/HomePage.feature.cs
@@ -20,8 +20,8 @@ namespace RedCrossWithBDD.Features
     [System.CodeDom.Compiler.GeneratedCodeAttribute("TechTalk.SpecFlow", "3.9.0.0")]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [NUnit.Framework.TestFixtureAttribute()]
-    [NUnit.Framework.DescriptionAttribute("Checking functionality of HomePage of the Application")]
-    public partial class CheckingFunctionalityOfHomePageOfTheApplicationFeature
+    [NUnit.Framework.DescriptionAttribute("HomePage")]
+    public partial class HomePageFeature
     {
         
         private TechTalk.SpecFlow.ITestRunner testRunner;
@@ -35,7 +35,7 @@ namespace RedCrossWithBDD.Features
         public virtual void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Features", "Checking functionality of HomePage of the Application", null, ProgrammingLanguage.CSharp, featureTags);
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Features", "HomePage", "To Validate functionality of HomePage", ProgrammingLanguage.CSharp, featureTags);
             testRunner.OnFeatureStart(featureInfo);
         }
         
@@ -73,55 +73,30 @@ namespace RedCrossWithBDD.Features
             testRunner.CollectScenarioErrors();
         }
         
-        [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("To validate whether user is on Landing page of the application of the application" +
-            "")]
-        [NUnit.Framework.CategoryAttribute("HomePage")]
-        public void ToValidateWhetherUserIsOnLandingPageOfTheApplicationOfTheApplication()
+        public virtual void FeatureBackground()
         {
-            string[] tagsOfScenario = new string[] {
-                    "HomePage"};
-            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("To validate whether user is on Landing page of the application of the application" +
-                    "", null, tagsOfScenario, argumentsOfScenario, featureTags);
+#line 3
+#line hidden
 #line 4
-this.ScenarioInitialize(scenarioInfo);
+testRunner.Given("user is on Landing page of the application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-            if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
-            {
-                testRunner.SkipScenario();
-            }
-            else
-            {
-                this.ScenarioStart();
 #line 5
- testRunner.Given("user initialized the browser", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+testRunner.And("user managed the cookies", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 6
- testRunner.When("user navigated to the Application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line hidden
-#line 7
- testRunner.Then("user is on Landing page of the application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line hidden
-#line 8
- testRunner.And("user closed the browser", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-            }
-            this.ScenarioCleanup();
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("To validate whether user is able to signup to the Application with Valid details")]
+        [NUnit.Framework.DescriptionAttribute("User is able to signup to the Application with Valid details")]
         [NUnit.Framework.CategoryAttribute("HomePage")]
         [NUnit.Framework.CategoryAttribute("UserSignup")]
-        public void ToValidateWhetherUserIsAbleToSignupToTheApplicationWithValidDetails()
+        public void UserIsAbleToSignupToTheApplicationWithValidDetails()
         {
             string[] tagsOfScenario = new string[] {
                     "HomePage",
                     "UserSignup"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("To validate whether user is able to signup to the Application with Valid details", null, tagsOfScenario, argumentsOfScenario, featureTags);
-#line 12
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("User is able to signup to the Application with Valid details", null, tagsOfScenario, argumentsOfScenario, featureTags);
+#line 9
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
@@ -131,43 +106,40 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 13
- testRunner.Given("user is on Landing page of the application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 3
+this.FeatureBackground();
 #line hidden
-#line 14
- testRunner.And("user managed the cookies", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 10
+ testRunner.Given("user clicked on My Account button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 15
- testRunner.And("user clicked on My Account button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 16
+#line 11
  testRunner.And("Login dialogue box appears", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 17
+#line 12
  testRunner.And("user clicked on Create your account Button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 18
+#line 13
  testRunner.And("Signup dialogue box appears", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 19
+#line 14
  testRunner.When("user entered Valid FirstName", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 20
+#line 15
  testRunner.And("user entered Valid LastName", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 21
+#line 16
  testRunner.And("user entered Valid Email", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 22
+#line 17
  testRunner.And("user entered Valid Password", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 23
+#line 18
  testRunner.Then("user clicked on Create My Account button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 24
+#line 19
  testRunner.And("user is able to signup successfully", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 25
+#line 20
  testRunner.And("user closed the browser", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
@@ -175,17 +147,17 @@ this.ScenarioInitialize(scenarioInfo);
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("To validate whether the user is able to login with valid credentials")]
+        [NUnit.Framework.DescriptionAttribute("User is able to login with valid credentials")]
         [NUnit.Framework.CategoryAttribute("HomePage")]
         [NUnit.Framework.CategoryAttribute("UserLogin")]
-        public void ToValidateWhetherTheUserIsAbleToLoginWithValidCredentials()
+        public void UserIsAbleToLoginWithValidCredentials()
         {
             string[] tagsOfScenario = new string[] {
                     "HomePage",
                     "UserLogin"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("To validate whether the user is able to login with valid credentials", null, tagsOfScenario, argumentsOfScenario, featureTags);
-#line 29
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("User is able to login with valid credentials", null, tagsOfScenario, argumentsOfScenario, featureTags);
+#line 24
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
@@ -195,31 +167,28 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 30
- testRunner.Given("user is on Landing page of the application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 3
+this.FeatureBackground();
 #line hidden
-#line 31
- testRunner.And("user managed the cookies", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 25
+ testRunner.Given("user clicked on My Account button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 32
- testRunner.And("user clicked on My Account button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 33
+#line 26
  testRunner.And("Login dialogue box appears", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 34
+#line 27
  testRunner.When("user entered Valid EmailID", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 35
+#line 28
  testRunner.And("user entered Valid UserPassword", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 36
+#line 29
  testRunner.Then("user click on Login Button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 37
+#line 30
  testRunner.And("user logged in successfully", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 38
+#line 31
  testRunner.And("user closed the browser", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             }
@@ -235,7 +204,7 @@ this.ScenarioInitialize(scenarioInfo);
                     "HomePage"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("To validate the menu bar buttons functionality", null, tagsOfScenario, argumentsOfScenario, featureTags);
-#line 59
+#line 34
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
@@ -245,14 +214,14 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 60
- testRunner.Given("user is on Landing page of the application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 3
+this.FeatureBackground();
 #line hidden
-#line 61
- testRunner.And("user managed the cookies", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 62
+#line 35
  testRunner.When("user clicked on menu buttons", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 36
+ testRunner.Then("user closed the browser", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
